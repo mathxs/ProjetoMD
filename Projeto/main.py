daa166668430fee14b04b045d6010156a990f304
mathxs <matheusdsantosp@gmail.com>
2019-11-10 15:50:06 -0300

    experimento 1

--- Projeto/main.py
+++ Projeto/main.py
@@ -2,18 +2,26 @@ import librosa
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data , fs = librosa.load (r'TREINAMENTO/6a66.wav', None)
-duracao_total = data.shape[0] / fs
-intervalo = 1
-dados_p_seg = {}
-
-for i,ini in enumerate(range(0, data.shape[0], fs*intervalo)):
-    print(i)
-    dados_p_seg[i] = pd.Series(data[ini:(ini + fs*intervalo)])
-
-dados_p_seg[0].plot()
-dados_p_seg[1].plot()
-
-plt.savefig('teste.png')
 #para compilar instale as bibliotecas pandas, librosa, matplotlib e atualize o colorama. (pip3 install ...)
 #python3 main.py
+
+def salvarPNGeWAV(dados):
+    dados_p_seg[dados].plot().get_figure().savefig('saida/' + str(dados) + '.png')
+    plt.close()
+    librosa.output.write_wav('saida/' + str(dados) + '.wav', dados_p_seg[dados].to_numpy(),fs)
+
+data , fs = librosa.load (r'TREINAMENTO/6a66.wav', None)
+print(fs)
+print(data.shape[0])
+duracao_total = data.shape[0] / fs
+intervalo = 2.130430839
+quebra_arquivo = int(fs*intervalo)
+print(quebra_arquivo)
+dados_p_seg = {}
+
+for i,ini in enumerate(range(0, data.shape[0], quebra_arquivo)):
+    print(i)
+    dados_p_seg[i] = pd.Series(data[ini:(ini + quebra_arquivo)])
+    salvarPNGeWAV(i)
+
+
